56a05d8500c9cfaf55fec98090f43c6da3944d5c
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2024-11-05 10:19:37 +0700

    initial commit

---
+++ src/main/java/com/nurmanhilman/eventbrite/EventbriteApplication.java
@@ -0,0 +1,13 @@
+package com.nurmanhilman.eventbrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventbriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventbriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurmanhilman/eventbrite/EventbriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurmanhilman.eventbrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
