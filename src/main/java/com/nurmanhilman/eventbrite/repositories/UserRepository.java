3943df2b30cdef60178df7a0e1229c00afe423f3
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2024-11-08 16:15:10 +0700

    added signup, login and jwt mechanism

---
+++ src/main/java/com/nurmanhilman/eventbrite/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nurmanhilman.eventbrite.repositories;
+
+import com.nurmanhilman.eventbrite.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
